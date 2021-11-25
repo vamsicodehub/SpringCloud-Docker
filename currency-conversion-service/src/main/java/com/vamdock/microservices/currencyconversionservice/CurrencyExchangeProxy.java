4e9d719a8c6306916ae0a94bcdebe2dbcc3d378f
hhh <hhh@Nagavamsi-APS>
2021-11-25 20:11:13 +0530

    added dockerfile for the project

--- currency-conversion-service/src/main/java/com/vamdock/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/vamdock/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -1,10 +1,11 @@
 package com.vamdock.microservices.currencyconversionservice;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
+@EnableRetry
 //@FeignClient(name="currency-exchange", url="localhost:8000")
 @FeignClient(name="currency-exchange")
 public interface CurrencyExchangeProxy {
